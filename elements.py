f09e2f5f8903666abf885512ee94af37000dea0d
CinDy <cincin_sripetch@hotmail.com>
2014-11-30 15:19:54 +0700

    add image object

--- elements.py
+++ elements.py
@@ -30,6 +30,7 @@ class ButtonL(ButtonR):
         self.color = 'green'
         return self.color
 """
+#################################################
 class Alphabet(object):
     def __init__(self,pos):
         self.pos = pos
@@ -55,4 +56,37 @@ class Alphabet(object):
 
         self.alpha_image = pygame.font.SysFont("Padauk",60).render(self.value,1,fontColor)
         surface.blit(self.alpha_image,self.pos)
+####################################################
+class Background(object):
+    def __init__(self):
+        (self.x, self.y) = (0, 0)
+        self.images = ('res/...') # images tuple hold all image
+        self.pic = 'res/...' # first background image
+
+    def change_image(self):
+        if self.count < len(self.images)-1:
+    #        print len(self.images),self.count
+            self.count += 1
+        else:
+            self.count = 1
+   #     print self.images[self.count],self.count
+        self.pic = self.images[self.count]
+        
+   #     if self.pic == "res/Nyan-cat.jpg":
+   #         self.x = -200
+   #     else :
+   #         self.x = 0
+
+
+    #    print self.pic
+
+    def render(self,surface):
+        self.img =pygame.image.load(self.pic)
+    #    if self.pic == "res/space.jpg":
+    #        self.img =pygame.transform.scale(self.img,(800,600))
+      #  self.time += pygame.time.Clock().get_time()
+      #  if self.time/1000.0 > 3000.0:
+      #      self.change_image()
+        surface.blit(self.img,(self.x,self.y))
+
 
